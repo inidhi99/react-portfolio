ce23540b9d11e4ab5fa968da4a8185be34d5a98a
nidhi <inidhi99@gmail.com>
2022-12-14 11:33:58 -0500

    added footer.js

---
+++ portfolio/src/components/Footer.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+function Footer() {
+  return (
+    <div className="container">
+      <footer id="main-footer">
+        <div class="icons">
+
+          <a href="https://github.com/inidhi99">
+            <i class="fab fa-github fa-2x"></i>
+          </a>
+        </div>
+      </footer>
+    </div>
+  );
+}
+
+export default Footer;
